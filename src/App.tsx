e4b3a8fdfc0380602fc8c2976bcb26fe08552781
Gbenga Olaseni <gj.olaseni@gmail.com>
2024-11-07 15:08:16 -0800

    Added vite-plugin-svgr and config it

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
 
-import './App.css';
-import { Button } from './components/ui/button';
+import SomeIcon from "./assets/user.svg?react";
 
 function App() {
-
+  console.log(SomeIcon); // Should print a React component function or class
 
   return (
     <>
-      <h1 className='mb-5'> shadcn/UI</h1>
-       <div className='mb-5'></div>
-       <Button>Primary</Button>
-       <Button variant='gbenga'>Primary</Button>
+      <h1 className="mb-5"> shadcn/UI</h1>
+      <div className="mb-5"></div>
+
+      {/* <Button variant='default' icon= {someIconUrl}>default</Button>
+       <Button variant='destructive' icon= {someIconUrl}>destructive</Button>
+       <Button variant='outline' icon={reactIcon}>outline</Button>
+       <Button variant='ghost' icon= {someIconUrl}>ghost</Button> */}
+
+      <Button variant="link" icon={<SomeIcon />}>
+        link
+      </Button>
     </>
   );
 }
