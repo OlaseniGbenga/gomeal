7d5286c88c14471c776122d129bdd1a68522c081
Gbenga Olaseni <gj.olaseni@gmail.com>
2024-11-12 01:17:09 +0100

    working on header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import "./App.css";
+//componenet
 import { Button } from "./components/ui/button";
 import Search from "./components/forms/Search";
-import { useState } from "react";
+import { Box } from "./components/ui/box";
+//react
+import { useEffect, useState } from "react";
+//Asset
 import SomeIcon from "./assets/user.svg?react";
 import Logo from "./assets/Logo.svg?react";
 import MapMarker from "./assets/map-marker-alt.svg?react";
 import SearchIcon from "./assets/Search.svg?react";
 import Hamburger from "./assets/burger-menu-svgrepo-com.svg?react";
-import { Box } from "./components/ui/box";
+//framer
+import { motion } from "framer-motion";
 
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
@@ -19,13 +24,82 @@ function App() {
   const handlecurrentLocation = (e: React.ChangeEvent<HTMLInputElement>) => {
     setCurrentLocation(e.target.value);
   };
+
+  const [isToggled, setIsToggled] = useState(false);
+
+  useEffect(() => {
+    // Get the <html> element
+    const htmlElement = document.documentElement;
+
+    // Apply overflow-y: hidden if state is true
+    if (isToggled) {
+      htmlElement.style.overflowY = 'hidden';
+    } else {
+      // Otherwise, reset to default (or use an empty string to restore)
+      htmlElement.style.overflowY = '';
+    }
+
+    // Cleanup function to reset on component unmount or state change
+    return () => {
+      htmlElement.style.overflowY = '';
+    };
+  }, [isToggled]); 
+
+  const toggleHeight = () => {
+    setIsToggled(!isToggled);
+
+  };
+  //m-auto max-w-[1366px] justify-between  px-[100px]  md:px-[50px] sm:px-[20px]  w-full
   return (
     <>
-      <Box className="flex justify-between ">
-        <div className="2xl:h-[40px] w-full flex justify-between 2xl:flex-col 2xl:items-start gap-y-2  items-center  overflow-hidden ">
-          <div className="h-[38px]">
-            <Logo />
+      <Box className="w-full bg-white    fixed flex justify-between flex-col overflow-hidden ">
+        <div className="justify-between w-full  overflow-hidden flex    ">
+          <div className=" py-8 w-full flex justify-between 2xl:flex-col 2xl:items-start gap-y-2 items-center ">
+            <div className=" h-[38px]">
+              <Logo />
+            </div>
+            {/*edit*/}
+            <div className=" 2xl:hidden  w-full flex justify-between">
+              <div className="2xl:flex-col 2xl:items-start gap-y-2 flex items-center">
+                <p className="font-bold text-[18px] ">Deliver to:</p>
+
+                <Search
+                  icon={<MapMarker />}
+                  placeholder="Current Location"
+                  className="w-[180px] border-white "
+                  searchTerm={currentLocation}
+                  handleSearchChange={handlecurrentLocation}
+                />
+                <p className="font-bold text-[18px]">
+                  Mohammadpur Bus Stand, Dhaka
+                </p>
+              </div>
+              <div className="2xl:flex-col flex gap-2">
+                <Search
+                  icon={<SearchIcon />}
+                  placeholder="Search Food"
+                  className="placeholder:font-bold border-white placeholder:text-black w-[150px]"
+                  searchTerm={searchTerm}
+                  handleSearchChange={handleSearchChange}
+                />
+
+                <Button
+                  className="m-[2px]"
+                  left={true}
+                  variant="default"
+                  icon={<SomeIcon />}
+                >
+                  Login
+                </Button>
+              </div>
+            </div>
           </div>
+
+          <div className=" h-[38px] hidden 2xl:inline-block py-8">
+            <Hamburger onClick={toggleHeight} className="cursor-pointer" />
+          </div>
+        </div>
+        <div className={`hidden w-full h-screen   ${isToggled ? "2xl:flex 2xl:flex-col" : ""}`}>
           <div className="2xl:flex-col 2xl:items-start gap-y-2 flex items-center">
             <p className="font-bold text-[18px] ">Deliver to:</p>
 
@@ -48,8 +122,9 @@ function App() {
               searchTerm={searchTerm}
               handleSearchChange={handleSearchChange}
             />
+
             <Button
-              className="m-[2px]"
+              className="m-[2px] w-[150px]"
               left={true}
               variant="default"
               icon={<SomeIcon />}
@@ -58,10 +133,21 @@ function App() {
             </Button>
           </div>
         </div>
+      </Box>
+      <Box className="bg-red-600 pt-[200px] ">
+        <div className="h-40 w-full bg-green-800">
 
-        <div className="h-[38px] hidden 2xl:inline-block">
-       
-          <Hamburger  className="cursor-pointer"/>
+        </div>
+      </Box>
+      <Box className="bg-yellow-600  ">
+        <div className="h-40 w-full bg-green-800">
+
+        </div>
+      </Box>
+
+      <Box className="bg-red-600 ">
+        <div className="h-40 w-full bg-green-800">
+
         </div>
       </Box>
     </>
--- src/components/ui/box.tsx
+++ src/components/ui/box.tsx
@@ -1,13 +1,13 @@
 import * as React from "react";
 
 import { cn } from "@/lib/utils";
-
+//
 const Box = React.forwardRef<HTMLDivElement, React.ComponentProps<"div">>(
   ({ className, ...props }, ref) => {
     return (
       <div
         className={cn(
-          " md:px-[50px] sm:px-[20px] max-w-[1366px] m-auto  px-[100px] py-[10px]  font-open-sans",
+          "   px-[100px] md:px-[50px] sm:px-[20px]   py-[10px]  font-open-sans",
           className
         )}
         ref={ref}
