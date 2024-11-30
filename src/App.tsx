0a50cc6cd89561440bbd89ffd06538be584775ed
Gbenga Olaseni <gj.olaseni@gmail.com>
2024-11-30 07:32:11 +0100

    completed footer

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,15 @@ import HowDoesItWork from "@/features/landingPage/howDoesItWork";
 import LandingPageCarousel from "@/features/landingPage/landingPageCarousel";
 import DownloadApp from "@/features/landingPage/DownloadApp";
 import { Box } from "@/components/ui/box";
-
+import Search from "./components/forms/Search";
+import { Button } from "./components/ui/button";
+import { useState } from "react";
+import Envelop from '@/assets/envelope.svg?react'
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+  };
   return (
     <>
       <Header />
@@ -16,7 +23,7 @@ function App() {
       <HowDoesItWork />
       {/* <LandingPageCarousel /> */}
       <DownloadApp />
-      <Box className=" grid grid-cols-[60%,40%] gap-20 md:flex md:flex-col bg-[#212121] text-dark-white">
+      <Box className=" grid grid-cols-[60%,40%] gap-20 md:flex md:flex-col bg-[#212121] text-dark-white py-28">
         <div className="flex justify-between  md:flex-col gap-8">
           <div>
             <p className="font-bold text-white mb-4">Company</p>
@@ -26,20 +33,38 @@ function App() {
             <p>Blog</p>
           </div>
           <div>
-            <p  className="font-bold text-white mb-4">Contact</p>
+            <p className="font-bold text-white mb-4">Contact</p>
             <p>Help & Support</p>
             <p>Partner with us </p>
             <p>Ride with us</p>
           </div>
           <div>
-            <p  className="font-bold text-white mb-4">Terms & Conditions</p>
+            <p className="font-bold text-white mb-4">Terms & Conditions</p>
             <p>Refund & Cancellation</p>
             <p>Privacy Policy</p>
             <p>Cookie Policy</p>
           </div>
-         
         </div>
-        <div className=" h-8"> </div>
+        <div className=" h-8 ">
+          <p className="mb-4 font-bold">FOLLOW US</p>
+          <p>Receive exclusive offers in your mailbox</p>
+          <div className=" flex gap-2 items-center sm:flex-col sm:items-stretch ">
+            <Search
+              icon={<Envelop className="fill-secondary" />}
+              placeholder="Enter Your email"
+              className=" border-[#ADADAD] fill-white   sm:!w-full  bg-[#ADADAD]"
+              searchTerm={searchTerm}
+              handleSearchChange={handleSearchChange}
+            />
+
+            <Button
+              className="m-[2px] bg-primary text-white "
+              variant="secondary"
+            >
+              Find Food
+            </Button>
+          </div>
+        </div>
       </Box>
     </>
   );
