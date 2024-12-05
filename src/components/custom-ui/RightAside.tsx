9b21a66c70c26ffee3a4f1a954b292a836b07563
Gbenga Olaseni <gj.olaseni@gmail.com>
2024-12-05 11:22:41 +0100

    working on responsiveness of right and left sidebar

--- src/components/custom-ui/RightAside.tsx
+++ src/components/custom-ui/RightAside.tsx
@@ -7,57 +7,93 @@ import TopUp from "@/assets/Income.svg?react";
 import Transfer from "@/assets/Profit.svg?react";
 
 import { Button } from "../ui/button";
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
+import { useState } from "react";
 
-function RightAside(){
-    return(<>
-    <div className="bg-white  w-[400px] h-screen  flex flex-col gap-8 px-4  py-8">
-          <div className="flex justify-between items-start">
-            <div className="flex items-center gap-2">
-              <Chat className="fill-black w-6" />
-              <Notification className="fill-black w-6" />
-              <Setting className="fill-black w-6" />
-            </div>
-            <div className="bg-black h-8 w-8 rounded-[8px]"></div>
+function RightAside() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <>
+      <Menubar className="hidden">
+        <MenubarMenu>
+          <MenubarTrigger onClick={toggleMenu}>
+            {isOpen ? "File" : "hi"}{" "}
+            {/* Display 'File' when open, hamburger when closed */}
+          </MenubarTrigger>
+          <MenubarContent>
+            <MenubarItem>
+              New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+            </MenubarItem>
+            <MenubarItem>New Window</MenubarItem>
+            <MenubarSeparator />
+            <MenubarItem>Share</MenubarItem>
+            <MenubarSeparator />
+            <MenubarItem>Print</MenubarItem>
+          </MenubarContent>
+        </MenubarMenu>
+      </Menubar>
+      <div className="lg:hidden bg-white  w-[400px] h-screen  flex flex-col gap-8 px-4  py-8">
+        <div className="flex justify-between items-start">
+          <div className="flex items-center gap-2">
+            <Chat className="fill-black w-6" />
+            <Notification className="fill-black w-6" />
+            <Setting className="fill-black w-6" />
           </div>
-          <div>
-            <p className="font-bold">Coming soon</p>
-            <div
-              style={{ backgroundImage: `url(${Background})` }}
-              className="h-[100px] w-full bg-black rounded-[10px] overflow-hidden bg-cover flex items-center justify-between p-4"
-            >
-              <div className="bg-white p-2 rounded-[10px]">
-                <p>Balance</p>
-                <p className="font-bold text-[1.5rem]">$12.00</p>
-              </div>
-              <div className="flex flex-col items-center  ">
-                <span className="py-1 px-2  rounded-lg bg-white flex justify-center items-center">
-                  <TopUp className="w-6" />
-                </span>
-                <p className="text-white">Top up</p>
-              </div>
-              <div className="flex flex-col items-center ">
-                <span className="py-1 px-2 rounded-lg bg-white flex justify-center items-center">
-                  <Transfer className="w-6" />
-                </span>
-                <p className="text-white">Transfer</p>
-              </div>
+          <div className="bg-black h-8 w-8 rounded-[8px]"></div>
+        </div>
+        <div>
+          <p className="font-bold">Coming soon</p>
+          <div
+            style={{ backgroundImage: `url(${Background})` }}
+            className="h-[100px] w-full bg-black rounded-[10px] overflow-hidden bg-cover flex items-center justify-between p-4"
+          >
+            <div className="bg-white p-2 rounded-[10px]">
+              <p>Balance</p>
+              <p className="font-bold text-[1.5rem]">$12.00</p>
+            </div>
+            <div className="flex flex-col items-center  ">
+              <span className="py-1 px-2  rounded-lg bg-white flex justify-center items-center">
+                <TopUp className="w-6" />
+              </span>
+              <p className="text-white">Top up</p>
+            </div>
+            <div className="flex flex-col items-center ">
+              <span className="py-1 px-2 rounded-lg bg-white flex justify-center items-center">
+                <Transfer className="w-6" />
+              </span>
+              <p className="text-white">Transfer</p>
             </div>
           </div>
-          <div>
-            <p className="font-bold">Actions</p>
-            <div className="flex flex-col justify-between gap-2">
-              <Button className="border-solid border-primary border-[1px] shadow-none">
-                Payment method
-              </Button>
-              <Button className="border-solid border-primary border-[1px] shadow-none">
-                Send Feedback
-              </Button>
-              <Button className="border-solid border-primary border-[1px] shadow-none">
-                Sign out
-              </Button>
-            </div>
+        </div>
+        <div>
+          <p className="font-bold">Actions</p>
+          <div className="flex flex-col justify-between gap-2">
+            <Button className="border-solid border-primary border-[1px] shadow-none">
+              Payment method
+            </Button>
+            <Button className="border-solid border-primary border-[1px] shadow-none">
+              Send Feedback
+            </Button>
+            <Button className="border-solid border-primary border-[1px] shadow-none">
+              Sign out
+            </Button>
           </div>
-        </div></>)
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default RightAside
+export default RightAside;
--- src/components/layout/protected.tsx
+++ src/components/layout/protected.tsx
@@ -3,12 +3,15 @@ import LeftAside from "@/components/custom-ui/leftAside";
 import RightAside from "@/components/custom-ui/RightAside";
 
 function Protected() {
+
   return (
     <>
       <div className="flex max-w-[1366px] m-auto">
         <LeftAside />
         <Outlet />
         <RightAside />
+
+       
       </div>
     </>
   );
