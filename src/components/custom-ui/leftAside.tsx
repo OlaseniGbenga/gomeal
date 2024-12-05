244421952e4e4d1d145085f64fe16dbb51957b40
Gbenga Olaseni <gj.olaseni@gmail.com>
2024-12-05 18:57:33 +0100

    completed navigation responsiveness

--- src/components/custom-ui/leftAside.tsx
+++ src/components/custom-ui/leftAside.tsx
@@ -13,7 +13,7 @@ import PageMenu from "./pageMenu";
 function LeftAside() {
   return (
     <>
-      <div className="bg-white md:bg-yellow-600    w-[200px] h-screen  flex flex-col gap-8  items-center py-8 px-2">
+      <div className="bg-white md:bg-yellow-600    w-[200px] h-screen md:h-auto  flex flex-col gap-8  items-center py-8 px-2">
         <Logo className="w-[150px] bg-white rounded-sm p-2" />
 
         <PageMenu className="flex md:hidden"/>
--- src/components/layout/protected.tsx
+++ src/components/layout/protected.tsx
@@ -3,15 +3,21 @@ import LeftAside from "@/components/custom-ui/leftAside";
 import RightAside from "@/components/custom-ui/RightAside";
 
 function Protected() {
-
   return (
     <>
-      <div className="flex max-w-[1366px] m-auto bg-yellow-600  bg-white">
-        <LeftAside />
-        <Outlet  />
-        <RightAside />
+      <div className="hidden md:flex flex-col max-w-[1366px] m-auto bg-yellow-600  bg-white">
+        <div className="flex justify-between">
+          <LeftAside />
+          <RightAside />
+        </div>
 
-       
+        <Outlet />
+      </div>
+
+      <div className=" md:hidden flex  max-w-[1366px] m-auto bg-yellow-600  bg-white">
+        <LeftAside />
+        <Outlet />
+        <RightAside />
       </div>
     </>
   );
